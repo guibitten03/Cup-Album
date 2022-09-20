949091e31d426757d2efde7727fa65b9df6f8369
Henrique Hott <henriquehott@pm.me>
2022-09-20 12:57:56 -0300

    Interface Persistence implemented.

--- persistence/Persistence.py
+++ persistence/Persistence.py
@@ -1,4 +1,24 @@
 from abc import ABC, abstractmethod
 
+
 class IPersistence(ABC):
-	pass
+	
+	@abstractmethod
+	def insert():
+		pass
+
+	@abstractmethod
+	def modify():
+		pass
+
+	@abstractmethod
+	def delete():
+		pass
+
+	@abstractmethod
+	def search_by_id(id: int):
+		pass
+
+	@abstractmethod
+	def search_by_str(s: str):
+		pass
