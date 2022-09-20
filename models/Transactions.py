6700a0743bbce567b18946960585655cad8fafdb
guilherme8426 <guilhermefonseca8426@aluno.ufsj.edu.br>
2022-09-20 13:41:09 -0300

    1

---
+++ models/Transactions.py
@@ -0,0 +1,29 @@
+from Emtity import Emtity
+from Shop import Shop
+from Collector import Collector
+
+class Transactions():
+
+    def __init__(self, price: float) -> None:
+        self.__price__ = price
+
+    def set_price(self, price: float) -> None:
+        self.__price__ = price
+
+    def get_price(self) -> float:
+        return self.__price__
+
+    def buy(self, C: Collector, s: Shop, quantidade: int) -> str:
+
+        if(c.get_money < quantidade*self.get_price):
+            return f"Não foi possivel finalizar a compra, Colecionador {C.get_name} não possui dinheiro suficiente"
+    
+        c.set_money = c.get_money - quantidade*self.get_price
+        c.set_count_packages = c.get_count_packages + quantidade 
+
+        s.set_stock = s.set_stock - quantidade 
+
+    def trade(Collector, Collector, ):
+
+
+
