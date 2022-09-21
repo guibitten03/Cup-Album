bd65a5048fd2d100ccf13c6a1b4dacb9b86473c4
guilherme8426 <guilhermefonseca8426@gmail.com>
2022-09-20 21:05:57 -0300

    class Transaction

--- models/Transactions.py
+++ models/Transactions.py
@@ -15,7 +15,7 @@ class Transactions():
     def buy(self, c: Collector, s: Shop, quantidade: int) -> str:
 
         if(c.get_money < quantidade*self.get_price):
-            return f"Não foi possivel finalizar a compra, Colecionador {C.get_name} não possui dinheiro suficiente"
+            return f"Não foi possivel finalizar a compra, Colecionador {c.get_name} não possui dinheiro suficiente"
     
         c.set_money = c.get_money - quantidade*self.get_price
         c.set_count_packages = c.get_count_packages + quantidade 
