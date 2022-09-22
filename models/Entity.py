70fa8b531ef65387278779b174aa7d45a67fbcbb
Henrique Hott <henriquehott@pm.me>
2022-09-22 11:39:16 -0300

    UPDATE: Entity implementation using id per class type.

--- models/Entity.py
+++ models/Entity.py
@@ -1,12 +1,36 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
+from typing import Type
 
 class Entity(ABC):
-	id: int = 0
+	"""
+	Description
+	-----------
+	Entity represents the base class of the system.
+	It can not be instatiated and should be inherited by all classes in the models package.
+
+	It is responsable for setting the id of each obj instantiated.\n
+	The id is class related, so objects of differents class may have same id.
+
+	Example
+	-------
+	>>> class Foo(Ent):
+	...	def __init__(self) -> None:
+	...		super().__init__(Foo)
+	>>> class Bar(Ent):
+	...	def __init__(self) -> None:
+	...		super().__init__(Bar)
+	>>> objs = [ Foo(), Foo(), Bar(), Bar() ]
+	>>> for obj in objs:
+	...	print(obj.id)
+	0 1 0 1
+	"""
+	id_per_type = defaultdict(int)
 
 	@abstractmethod
-	def __init__(self) -> None:
-		self.id = Entity.id
-		Entity.id = Entity.id + 1
+	def __init__(self, t: Type) -> None:
+		self.id = Entity.id_per_type[t]
+		Entity.id_per_type[t] = Entity.id_per_type[t] + 1
 
 	@abstractmethod
 	def __str__() -> str:
