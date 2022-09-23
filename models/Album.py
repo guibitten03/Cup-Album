745f6819441eea688633d8cc3879a933ab53fa02
guibitten03 <bittenkourt.gmf@gmail.com>
2022-09-23 19:01:00 -0300

    UPDATE: Album class -> Menu

--- models/Album.py
+++ models/Album.py
@@ -1,11 +1,11 @@
 from xml.dom.minidom import Entity
 from Sticker import *
-from database.
+
 
 class Album(Entity):
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(Album)
 
         self.album = {}
         self.album_size = 0
@@ -20,6 +20,34 @@ class Album(Entity):
 
         self.album_size += 1
 
+    def get_album(self): return self.album
+    def get_album_size(self): return self.album_size
+    
+    def set_album(self, album): self.album = album
+    def set_album_size(self, size): self.album_size = size
+
+#     def menu(self):
+#         input = int(input('''Selecione uma opção:\n
+# 0 - Sair\n
+# 1 - Olhar album\n
+# 2 - Album está completo?'''))
+
+#         while True:
+#             match:
+#                 case 0:
+#                     break
+#                 case 1:
+#                     if self.album_size == 0:
+#                         print("Você ainda não colou figurinhas no album")
+#                     else:   
+#                         for team in self.album.keys():
+#                             for players in self.album[team]:
+#                                 print("{}: {}".format(self.album[team]))
 
     def __str__(self) -> str:
         return "{} stickers are in album\n".format(self.album_size) 
+    
+    
+if __name__ == "__main__":
+    album = Album()
+    print(album.album_size)
