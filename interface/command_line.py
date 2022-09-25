4771e5f09784273ad93f6760ca73fe376b990864
guilherme8426 <guilhermefonseca8426@gmail.com>
2022-09-25 10:53:42 -0300

    command line interface

--- interface/command_line.py
+++ interface/command_line.py
@@ -17,21 +17,100 @@ class command_lines():
         os.system("cls" if os.name=="nt" else "clear") or None
 
     @staticmethod
-    def message(mensagens: str, font: str = "slant") -> None:
+    def __message(mensagens: str, font: str = "slant") -> None:
         result = pyfiglet.figlet_format(mensagens, font = "slant")
         print(result)
 
     @staticmethod
-    def choice(options: list[str], cursor_style: tuple[str] = ("fg_red", "bold"), highlight_style = "standout") -> str:
-        command_lines.__clear()
-
-        mainMenu = TerminalMenu(options, menu_cursor_style = cursor_style, menu_highlight_style = (highlight_style,), )
+    def __choice(options: list[str], cursor_style: tuple[str] = ("fg_red", "bold"), highlight_style: str = "standout", title: str = '' ) -> int:
+        mainMenu = TerminalMenu(options, menu_cursor_style = cursor_style, menu_highlight_style = (highlight_style,), title= title)
         user_choice = mainMenu.show()
-        return options[user_choice]
+        return user_choice
 
-options = ['a', 'b', 'c', 'd']
+    @staticmethod
+    def __click_to_exit() -> None:
+        option = ['Exit']
+        command_lines.__choice(option)
 
-#if command_lines.choice(options) == 'a':
-#    print('deu certo')
-  
-command_lines.message("tete")
+    @staticmethod
+    def home():
+        
+        options = ['collector', 'sticker aubum', 'stikers', 'exit']
+        LOOP = True
+
+        while(LOOP):
+            command_lines.__clear() 
+            command_lines.__message("Fifa WC Album")
+            user_choice = command_lines.__choice(options, title = 'choose one option')
+
+            if user_choice == 0:
+                command_lines.__clear()
+                command_lines.Collector()
+
+            elif user_choice == 1:
+                command_lines.__clear()
+                command_lines.Sticker_aubum()
+
+            elif user_choice == 2:
+                command_lines.__clear()
+                command_lines.Stikers()
+
+            elif user_choice == 3:
+                command_lines.__clear()
+                LOOP = False
+    
+    @staticmethod
+    def Collector():
+        #uma opção para cada colecionador na permenencia, colocar um atributo com o nome de cada colecionador
+        options = ['colecionador1', 'colecionador2', 'colecionador3', 'ADD collector', 'Exit']
+        options_exit = len(options)-1
+        options_add = options_exit -1
+        LOOP = True
+
+        #command_lines.__clear() ##teste
+        #command_lines.__message('Collector') ##teste
+
+        
+        while LOOP:
+            command_lines.__clear()  #teste
+            command_lines.__message('Collector') #teste
+
+            user_choise = command_lines.__choice(options, title='choose a collector')    
+            
+            if user_choise == options_exit:
+                LOOP = False
+            
+            elif user_choise == options_add:
+                # cria e salva um collector
+                print('aqui')
+                command_lines.__click_to_exit()        
+
+            ## Função q seleciona um collector atraves do nome
+            else:
+                print(options[user_choise])
+                command_lines.__click_to_exit() 
+
+
+    @staticmethod
+    def Sticker_aubum():
+        options = []
+        LOOP = True
+
+        while LOOP:
+            command_lines.__clear()
+            command_lines.__message('sticker aubum')
+            command_lines.__click_to_exit()
+            LOOP = False
+
+    @staticmethod
+    def Stikers():
+        options = []
+        LOOP = True
+
+        while LOOP:
+            command_lines.__clear()
+            command_lines.__message('stikers')
+            command_lines.__click_to_exit()
+            LOOP = False
+
+command_lines.home()
