f43c92a6625cc537ba862c6ab0b526b4012f2eac
yan <yanccomp@gmail.com>
2022-09-30 18:38:05 -0300

    Collector class done

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from models.Collector import Collector
+
+
 def main():
+	print(Collector('yan'))
 	pass
 
 if __name__ == '__main__':
---
+++ models/Collector.py
@@ -0,0 +1,18 @@
+from models import Entity
+
+
+class Collector(Entity):
+
+    def __init__(self, name : str):
+        super.__init__(Collector)
+        self.name = name
+
+    def get_id(self):
+        return self.id
+    def get_name(self):
+        return self.name
+    def set_name(self,name : str):
+        self.name = name
+
+    def __str__(self):
+        return "Id: {},Name: {}".format(self.id, self.name)
