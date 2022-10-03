fd8c993f23bca537a4c1309623071e3a4ce7c292
guibitten03 <bittenkourt.gmf@gmail.com>
2022-10-03 13:36:44 -0300

    Sticker Persistence implemented

--- persistence/Sticker_Persistence.py
+++ persistence/Sticker_Persistence.py
@@ -1,20 +1,54 @@
+from models.Sticker import Sticker
 from persistence.Persistence import IPersistence
 
-class StickerPersistence():
-    #pra facilitar no menu deixa esses atributos estaticos 👍
-    teams = ['Brasil', 'Argentina', '...'] #lista com o nome dos times
-    position = ['atacante', 'meia-direita', 'goleiro', '...'] 
-
+class StickerPersistence(IPersistence):
     
-    def insert(self, x, y, z):
-        print("função insert")
+    stickers = dict()
 
-    def remove(self, x):
-        print("função remove")
+    @staticmethod
+    def insert(s: Sticker) -> None:
+        StickerPersistence.stickers[s.id] = s
+    
+    @staticmethod
+    def remove(id : int) -> None:
+        if id in StickerPersistence.stickers:
+            StickerPersistence.stickers.pop(id)
+    
+    @staticmethod
+    def modify(id : int, name : str) -> None:
+        if id in StickerPersistence.stickers:
+            StickerPersistence.stickers[id].name = name
 
-    def modify(self, team_modify: str = None, position_modify: str = None, name_modify: str = None):
-        print("função modify")
+    @staticmethod
+    def search_by_id(id : int) -> Sticker:
+        if id in StickerPersistence.stickers:
+            return StickerPersistence.stickers[id]
+        return None
 
-    def search_by_id(self, id):
-        print("função search")
-        return 1 #retorna o obj ou None se não existir
+    @staticmethod
+    def search_by_str(name : str) -> Sticker:        
+        for _, s in StickerPersistence.stickers.items():
+            if s.name == name:
+                return s
+        return None
+    
+    @staticmethod
+    def view_data()-> None:
+        for _, s in StickerPersistence.stickers.items():
+            print(s)
+    
+    @staticmethod
+    def save():
+        with open("collector.txt", "w") as f:
+            for _,s in StickerPersistence.stickers.items():
+                f.write("{},{}\n".format(s.id, s.name))
+
+    @staticmethod
+    def load():
+        StickerPersistence.stickers.clear()
+        with open("sticker.txt","a+") as f:
+            f.seek(0)
+            for line in f:
+                data = line.split(",")
+                c = Sticker(data[1].rstrip(),id = int(data[0]))
+                StickerPersistence.insert(c)
