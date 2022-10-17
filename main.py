bce05d069839cd503694d80968d482c9af6e7001
guilherme8426 <guilhermefonseca8426@aluno.ufsj.edu.br>
2022-10-17 11:01:21 -0300

    Final project

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from genericpath import exists
 from interface.command_line import command_lines
 from persistence.Album_Persistence import AlbumPersistence
 from persistence.Collector_Persistence import CollectorPersistence
 from persistence.Sticker_Persistence import StickerPersistence
 from persistence.Trade_Persistence import TradePersistence
-
+import os
 def main():
+
+	os.makedirs("data",exist_ok=True)
+
 	StickerPersistence.load()
 	AlbumPersistence.load()
 	CollectorPersistence.load()
